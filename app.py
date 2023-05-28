78861dde7c7d5feb3ac978d3fbc80863fb1660fa
ChristianBrandenburg <pinconspiracy@gmail.com>
2023-05-28 22:35:34 +0200

    initial python setup

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import os
+
+from flask import Flask, request, Response, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('FLASK_PORT', 5555))
+    app.run(host='0.0.0.0', port=port)
